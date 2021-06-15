a0e48127383ac8cf15d37e66feba74baeaf2d64f
gpetrov <g.petrov@artsofte.ru>
2021-06-15 16:43:18 +0500

    letters

--- mailbox/src/app/components/mailbox/mailbox.component.ts
+++ mailbox/src/app/components/mailbox/mailbox.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import { defer, from, Observable, of, Subscription, timer } from 'rxjs';
 import { Mail, MailService } from '../mail.service';
-
+import { interval } from 'rxjs';
+import { bufferCount, concatAll, concatMap, delay, map } from 'rxjs/operators';
 @Component({
   selector: 'app-mailbox',
   templateUrl: './mailbox.component.html',
@@ -10,23 +11,33 @@ import { Mail, MailService } from '../mail.service';
 })
 export class MailboxComponent implements OnInit, OnDestroy {
 
-  mails!: Mail[]
+  mails: Mail[] = []
   mailsSub!: Subscription; 
-  mail!: Mail;
+  newMail!: Mail[];
+  s2 = defer(() => of(new Date()));
 
   constructor( private mailService: MailService) { }
-
+  
   ngOnInit(): void {
-    this.mailsSub = this.mailService.getAll().subscribe(mails => {
-      this.mails = mails
-    })
+    this.mailsSub = this.mailService.getAll()
+    .pipe(
+      concatAll(),
+      bufferCount(1),
+      concatMap((arr, i) =>
+        timer(i > 0 ? 4000 : 0).pipe(
+          map(() => arr)
+        )
+      )
+    )
+    .subscribe(arr => this.mails = [...this.mails, ...arr])
   }
 
-  getMail(): void {
-    for (let i = 0; i < this.mails?.length; i++) {
-      this.mail = this.mails[i]
-      i++
-    }
+  getCurrentDate(el: any) : Date {
+    return el.date = new Date()
+  }
+   
+  removeMail(index: number): void {
+    delete this.mails[index]
   }
 
   ngOnDestroy() {
